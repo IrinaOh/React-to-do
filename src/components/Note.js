331741e35aa13a865a813992804c6f4178667da9
Irina O'Hara <iohara@rmoadmins-MacBook-Pro.local>
2019-04-15 13:54:43 -0600

    initial commit

---
+++ src/components/Note.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Note extends Component {
+
+  render() {
+    return (
+      <div className="note">
+
+      </div>
+    );
+  }
+}
+
+export default Note;
