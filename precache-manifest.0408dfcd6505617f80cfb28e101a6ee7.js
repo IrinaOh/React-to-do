be98ddb7065c5ace6ed4b9c9233b81b6a56d7845
Irina O'Hara <iohara@rmoadmins-MacBook-Pro.local>
2019-04-29 10:28:27 -0600

    Updates

---
+++ precache-manifest.0408dfcd6505617f80cfb28e101a6ee7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29c9bd2e73f3b4ca802d",
+    "url": "/React-to-do/static/js/runtime~main.0ebc5b07.js"
+  },
+  {
+    "revision": "e1ba8aa50fd108c86a82",
+    "url": "/React-to-do/static/js/main.91c4cbc0.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-to-do/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e1ba8aa50fd108c86a82",
+    "url": "/React-to-do/static/css/main.b994d258.chunk.css"
+  },
+  {
+    "revision": "5dde01c7d745e48f2c92167e5077cd23",
+    "url": "/React-to-do/index.html"
+  }
+];
